9351687c0c521e6eec3be83c09e603adb5cb6652
Ralph007 <Ralph@macbook-pro-2.local>
2016-09-16 11:11:54 +0800

    Initial Commit

---
+++ Test_WildPointer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Test_WildPointer
+//
+//  Created by Ralph007 on 16/9/16.
+//  Copyright © 2016年 Ralph007. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
